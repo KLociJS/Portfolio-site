c5ce676693d4e36288405f5117417562dbeee9fe
KLociJS <kisznerlorant21@gmail.com>
2023-09-11 18:00:57 +0200

    fix: Fix layout for separators

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -11,34 +11,36 @@ import { SiDotnet } from 'react-icons/si'
 export default function Landing() {
   return (
     <section className='landing'>
-          <div>
-            <h1 className='landing-h1'>Welcome,</h1>
-            <h2 className='landing-h2'>My name is Lóránt Kiszner.</h2>
-            <div className='contact-icons'>
-              <a href='https://www.linkedin.com/in/lorantkiszner/' rel="noopener noreferrer" target="_blank">
-                <BsLinkedin className='linkedin-logo'/>
-              </a>
-              <a href='https://github.com/KLociJS' rel="noopener noreferrer" target="_blank">
-                <BsGithub className='github-logo' />
-              </a>
-              <a href="mailto: kisznerlorant21@gmail.com">
-                <BiLogoGmail className='mail-logo' />
-              </a>
-            </div>
+      <div className='landing-container'>
+        <div>
+          <h1 className='landing-h1'>Welcome,</h1>
+          <h2 className='landing-h2'>My name is Lóránt Kiszner.</h2>
+          <div className='contact-icons'>
+            <a href='https://www.linkedin.com/in/lorantkiszner/' rel="noopener noreferrer" target="_blank">
+              <BsLinkedin className='linkedin-logo'/>
+            </a>
+            <a href='https://github.com/KLociJS' rel="noopener noreferrer" target="_blank">
+              <BsGithub className='github-logo' />
+            </a>
+            <a href="mailto: kisznerlorant21@gmail.com">
+              <BiLogoGmail className='mail-logo' />
+            </a>
           </div>
-          <div>
-            <img src={Image} alt='Developer working on laptop' className='developer-img'/>
-            <div className='icon-container'>
-              <AiFillHtml5 className='html-logo' />
-              <BiLogoCss3 className='css-logo' />
-              <BiLogoJavascript className='js-logo' />
-              <BiLogoReact className='react-logo' />
-              <BiLogoNodejs className='node-logo' />
-              <BiLogoMongodb className='mongo-logo' />
-              <SiDotnet className='dotnet-logo' />
-              <BiLogoPostgresql className='postgresql-logo' />
-            </div>
+        </div>
+        <div>
+          <img src={Image} alt='Developer working on laptop' className='developer-img'/>
+          <div className='icon-container'>
+            <AiFillHtml5 className='html-logo' />
+            <BiLogoCss3 className='css-logo' />
+            <BiLogoJavascript className='js-logo' />
+            <BiLogoReact className='react-logo' />
+            <BiLogoNodejs className='node-logo' />
+            <BiLogoMongodb className='mongo-logo' />
+            <SiDotnet className='dotnet-logo' />
+            <BiLogoPostgresql className='postgresql-logo' />
           </div>
-        </section>
+        </div>
+      </div>
+    </section>
   )
 }
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -8,13 +8,13 @@ export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false)
   return (
     <>
-        <SlMenu className='menu-icon' onClick={()=>setIsOpen(s=>!s)}/>
-        <nav className={`nav-bar ${isOpen ? 'open' : ''}`}>
-          <a href='#about'>About</a>
-          <a href='#skills'>Skills</a>
-          <a href='#featured'>Projects</a>
-          <a href='#contact'>Contact</a>
-        </nav>
+      <SlMenu className='menu-icon' onClick={()=>setIsOpen(s=>!s)}/>
+          <nav className={`nav-bar ${isOpen ? 'open' : ''}`}>
+            <a href='#about'>About</a>
+            <a href='#skills'>Skills</a>
+            <a href='#featured'>Projects</a>
+            <a href='#contact'>Contact</a>
+          </nav>
     </>
   )
 }
