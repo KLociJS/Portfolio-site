5607ccd793460e14e73078c3c5d7dfa8acbbd981
KLociJS <kisznerlorant21@gmail.com>
2023-09-12 11:50:23 +0200

    feat: Add section separator

---
+++ src/Components/Separator/BottomSeparator.js
@@ -0,0 +1,7 @@
+import './Separator.css'
+
+export default function BottomSeparator() {
+  return (
+    <div className='bottom-separator'></div>
+  )
+}
---
+++ src/Components/Separator/TopSeparator.js
@@ -0,0 +1,7 @@
+import './Separator.css'
+
+export default function TopSeparator() {
+  return (
+    <div className='top-separator'></div>
+  )
+}
