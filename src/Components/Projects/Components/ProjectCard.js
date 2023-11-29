0429551d154d4bb52bb719f13e51c3c4e4b09de0
KLociJS <kisznerlorant21@gmail.com>
2023-11-29 18:34:34 +0100

    feat: Implement Project card partially

--- src/Components/Projects/Components/ProjectCard.js
+++ src/Components/Projects/Components/ProjectCard.js
@@ -1,11 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import { createPortal } from "react-dom";
 
-export default function ProjectCard({children, title, githubLink}) {
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
+
+export default function ProjectCard({
+  thumbnail,
+  title,
+  projectType,
+  description,
+  githubLink,
+  images,
+  parentRef,
+}) {
+  const [showModal, setShowModal] = useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  console.log(parentRef);
+
+  const closeModal = () => {
+    setShowModal(false);
+    setCurrentImageIndex(0); // Reset image index when closing modal
+  };
+
+  const prevSlide = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
+
+  const nextSlide = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const transformValue = `translateX(calc(-${100 * currentImageIndex}%))`;
+
   return (
     <div className='project-card'>
-        {children}
-        <h2>{title}</h2>
-        <a href={githubLink} rel="noopener noreferrer" target="_blank" className='card-button'>Github</a>
+      <p className='project-type'>{projectType}</p>
+      <h2 className='project-title'>{title}</h2>
+      <p className='project-description'>{description}</p>
+      {thumbnail}
+      <button onClick={openModal} className='card-button'>
+        Learn more
+      </button>
+      {showModal &&
+        createPortal(
+          <div className='modal'>
+            <button onClick={closeModal} className='close-button'>
+              <IoClose className='close-icon' />
+            </button>
+            <p className='project-type'>{projectType}</p>
+            <h2 className='project-title'>{title}</h2>
+            <p className='project-description'>{description}</p>
+            <div className='slider-wrapper'>
+              <div className='slider' style={{ transform: transformValue }}>
+                {images.map((image, index) => (
+                  <div className='slide'>
+                    <img
+                      key={index}
+                      src={image}
+                      alt={`Project ${index}`}
+                      className='modal-image'
+                    />
+                  </div>
+                ))}
+              </div>
+              {currentImageIndex > 0 ? (
+                <button
+                  onClick={prevSlide}
+                  disabled={currentImageIndex === 0}
+                  className='prev-button'
+                >
+                  <IoIosArrowBack className='prev-icon' />
+                </button>
+              ) : null}
+              {currentImageIndex < images.length - 1 ? (
+                <button
+                  onClick={nextSlide}
+                  disabled={currentImageIndex + 1 === images.length}
+                  className='next-button'
+                >
+                  <IoIosArrowForward className='next-icon' />
+                </button>
+              ) : null}
+            </div>
+          </div>,
+          parentRef
+        )}
     </div>
-  )
+  );
 }
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,39 +1,75 @@
-import ProjectCard from './Components/ProjectCard'
-import PortfolioThumbnail from './images/portfolio.png'
-import LearningAppThumbnail from './images/learning.png'
-import EventureThumbnail from './images/calendar.png'
-import './Projects.css'
+import { useEffect, useRef, useState } from "react";
+import ProjectCard from "./Components/ProjectCard";
+import "./Projects.css";
 
+// Thumbnails
+import EventureThumbnail from "./images/calendar.png";
+import LearningAppThumbnail from "./images/learning.png";
+import PortfolioThumbnail from "./images/portfolio.png";
+
+// WDN images
+import WdnHome from "../../Assets/wdn-home.png";
+import WdnNote from "../../Assets/wdn-note.png";
+import WdnProfile from "../../Assets/wdn-profile.png";
 
 export default function Projects() {
+  const projectSectionRef = useRef(null);
+  const [element, setElement] = useState(null);
+
+  useEffect(() => {
+    setElement(projectSectionRef.current);
+  }, []);
+
   return (
-    <section id='projects'>
-        <div className='project-section-container'>
-            <h2 className='section-header-light'>My projects</h2>
-            <div className='project-container'>
-                <ProjectCard title='Portfolio project' githubLink='https://github.com/KLociJS/Portfolio-site'>
-                    <img 
-                        src={PortfolioThumbnail} 
-                        alt='Portfolio website thumbnail' 
-                        className='thumbnail'
-                    />
-                </ProjectCard>
-                <ProjectCard title='Learning app' githubLink='https://github.com/KLociJS/LearningApp'>
-                    <img 
-                        src={LearningAppThumbnail} 
-                        alt='Learning app website thumbnail' 
-                        className='thumbnail'
-                    />
-                </ProjectCard>
-                <ProjectCard title='Eventure' githubLink='https://github.com/KLociJS/Advanced-team-project'>
-                    <img 
-                        src={EventureThumbnail} 
-                        alt='Eventure website thumbnail' 
-                        className='thumbnail'
-                    />
-                </ProjectCard>
-            </div>
+    <section id='projects' ref={projectSectionRef}>
+      <div className='project-section-container'>
+        <h2 className='section-header-light'>My projects</h2>
+        <div className='project-container'>
+          <ProjectCard
+            parentRef={element}
+            projectType='Solo Project'
+            title='Portfolio project'
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard "
+            githubLink='https://github.com/KLociJS/Portfolio-site'
+            images={[WdnHome, WdnNote, WdnProfile]}
+            thumbnail={
+              <img
+                src={PortfolioThumbnail}
+                alt='Portfolio website thumbnail'
+                className='thumbnail'
+              />
+            }
+          ></ProjectCard>
+          <ProjectCard
+            parentRef={element}
+            projectType='Solo Project'
+            title='Learning app'
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard "
+            githubLink='https://github.com/KLociJS/LearningApp'
+            thumbnail={
+              <img
+                src={LearningAppThumbnail}
+                alt='Learning app website thumbnail'
+                className='thumbnail'
+              />
+            }
+          ></ProjectCard>
+          <ProjectCard
+            parentRef={element}
+            projectType='Team Project'
+            title='Eventure'
+            description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard "
+            githubLink='https://github.com/KLociJS/Advanced-team-project'
+            thumbnail={
+              <img
+                src={EventureThumbnail}
+                alt='Eventure website thumbnail'
+                className='thumbnail'
+              />
+            }
+          ></ProjectCard>
         </div>
+      </div>
     </section>
-  )
+  );
 }
