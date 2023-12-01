e0bb450c2a575c054c5addd52aad47f21e3e3069
KLociJS <kisznerlorant21@gmail.com>
2023-12-01 12:12:42 +0100

    fix: Fix navbar on mobile

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,20 +1,29 @@
-import { useState } from 'react'
-import './NavBar.css'
+import { useState } from "react";
+import "./NavBar.css";
 
 // Icons
-import { SlMenu } from 'react-icons/sl'
+import { IoIosMenu, IoMdClose } from "react-icons/io";
 
 export default function NavBar() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen((s) => !s);
+  };
   return (
     <>
-      <SlMenu className='menu-icon' onClick={()=>setIsOpen(s=>!s)}/>
-          <nav className={`nav-bar ${isOpen ? 'open' : ''}`}>
-            <a href='#about'>About</a>
-            <a href='#skills'>Skills</a>
-            <a href='#projects'>Projects</a>
-            <a href='#contact'>Contact</a>
-          </nav>
+      <div className='nav-container'>
+        {isOpen ? (
+          <IoMdClose className='menu-icon' onClick={toggleMenu} />
+        ) : (
+          <IoIosMenu className='menu-icon' onClick={toggleMenu} />
+        )}
+      </div>
+      <nav className={`nav-bar ${isOpen ? "open" : ""}`}>
+        <a href='#about'>About</a>
+        <a href='#skills'>Skills</a>
+        <a href='#projects'>Projects</a>
+        <a href='#contact'>Contact</a>
+      </nav>
     </>
-  )
+  );
 }
